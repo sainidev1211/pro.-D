8b13ab6ebd4b40adf983fac23c85e197ecc1737f
Dev Saini <24BAI70170@cuchd.in>
2026-09-16 23:09:38 +0530

    Time: 174 ms (48.47%), Space: 25.7 MB (10.1%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.py
@@ -0,0 +1,30 @@
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val):
+        
+        if not self.stack:
+            self.stack.append((val, val))
+        else:
+            current_min = self.stack[-1][1]
+            self.stack.append((val, min(val, current_min)))
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][0]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][1]
